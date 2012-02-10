92965668a263c0ba325cf1f5e650e23d4f3cf245
mhall <none@none>
2012-02-10 10:46:06 +0000

    Reworked the UI to be more consistent.

--- src/pyquest/helpers/ui.py
+++ src/pyquest/helpers/ui.py
@@ -10,9 +10,9 @@ from pywebtools import ui
 
 def main_menu(current, survey, request):
     return ui.menu([('back', [Markup('&larr;'),  ' All my surveys'], {'href': request.route_url('survey'), 'class': 'no-tab', 'style': 'font-weight:normal;'}),
-                    ('overview', 'Overview', {'href': request.route_url('survey.view', sid=survey.id)}),
+                    ('survey', 'Survey', {'href': request.route_url('survey.view', sid=survey.id)}),
+                    ('pages', 'Pages', {'href': request.route_url('survey.qsheet', sid=survey.id)}),
                     ('data', 'Data', {'href': request.route_url('survey.data', sid=survey.id)}),
-                    ('edit', 'Edit', {'href': request.route_url('survey.edit', sid=survey.id)}),
                     ('preview', 'Preview', {'href': request.route_url('survey.preview', sid=survey.id)}),
                     ('results', 'Results', {'href': request.route_url('survey.results', sid=survey.id)})],
                    current,
