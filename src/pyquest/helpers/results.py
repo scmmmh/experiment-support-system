5bf76aa22761cb87bb390161196e2f3ec90dbd26
mhall <none@none>
2012-02-13 17:53:11 +0000

    Added a summary for all result types.

--- src/pyquest/helpers/results.py
+++ src/pyquest/helpers/results.py
@@ -42,35 +42,72 @@ def summary_single_in_list(qsheet, data_item, question, question_attr, participa
         total = reduce(lambda a, b: a + b, counts.values())
         for value in question_attr['values']:
             result.append('%s: %i' % (value, counts[value]))
-        result.append('Total: %s' % (total))
+        result.append('Total: %s of %i' % (total, len(participants)))
         return '; '.join(result)
     else:
         return ''
 
+def summary_multi_in_list(qsheet, data_item, question, question_attr, participants):
+    if len(participants) > 0:
+        counts = {}
+        for value in question_attr['values']:
+            counts[value] = 0
+        total = 0
+        for participant in participants:
+            if qsheet in participant and data_item in participant[qsheet]['items'] \
+                    and question in participant[qsheet]['items'][data_item]:
+                tmp = participant[qsheet]['items'][data_item][question]
+                if isinstance(tmp, list):
+                    for value in tmp:
+                        if value in counts:
+                            counts[value] = counts[value] + 1
+                else:
+                    if tmp in counts:
+                        counts[tmp] = counts[tmp] + 1
+                total = total + 1
+        result = []
+        for value in question_attr['values']:
+            result.append('%s: %i' % (value, counts[value]))
+        result.append('Total: %s of %i' % (total, len(participants)))
+        return '; '.join(result)
+    else:
+        return ''
+
+def summary_count(qsheet, data_item, question, participants):
+    if len(participants) > 0:
+        count = 0
+        for participant in participants:
+            if qsheet in participant and data_item in participant[qsheet]['items'] \
+                    and question in participant[qsheet]['items'][data_item]:
+                count = count + 1
+        return '%i of %i' % (count, len(participants))
+    else:
+        return '-'
+    
 def question_summary(qsheet, data_item, question, question_attr, participants):
     if question_attr['type'] == 'boolean':
         return summary_boolean(qsheet, data_item, question, participants)
     elif question_attr['type'] == 'single_in_list':
         return summary_single_in_list(qsheet, data_item, question, question_attr, participants)
     elif question_attr['type'] == 'multi_in_list':
-        pass
+        return summary_multi_in_list(qsheet, data_item, question, question_attr, participants)
     elif question_attr['type'] == 'number':
-        pass
+        return summary_count(qsheet, data_item, question, participants)
     elif question_attr['type'] == 'unicode':
-        pass
+        return summary_count(qsheet, data_item, question, participants)
     elif question_attr['type'] == 'email':
-        pass
+        return summary_count(qsheet, data_item, question, participants)
     elif question_attr['type'] == 'url':
-        pass
+        return summary_count(qsheet, data_item, question, participants)
     elif question_attr['type'] == 'date':
-        pass
+        return summary_count(qsheet, data_item, question, participants)
     elif question_attr['type'] == 'time':
-        pass
+        return summary_count(qsheet, data_item, question, participants)
     elif question_attr['type'] == 'datetime':
-        pass
+        return summary_count(qsheet, data_item, question, participants)
     elif question_attr['type'] == 'month':
-        pass
+        return summary_count(qsheet, data_item, question, participants)
     elif question_attr['type'] == 'all_in_list':
-        pass
+        return summary_count(qsheet, data_item, question, participants)
     else:
-        return '-'
+        return summary_count(qsheet, data_item, question, participants)
