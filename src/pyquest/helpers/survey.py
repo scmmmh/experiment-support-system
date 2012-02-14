f5499e3dc72526bc5117df9ad76daa4a14bd65e7
mhall <none@none>
2012-02-14 13:01:42 +0000

    Added fancy status display to all pages.

--- src/pyquest/helpers/survey.py
+++ src/pyquest/helpers/survey.py
@@ -5,16 +5,41 @@ Created on 14 Feb 2012
 @author: mhall
 '''
 
-def status(stat):
+from genshi.builder import tag
+
+def status(stat, lower_case=False):
     if stat == 'develop':
-        return 'In Development'
+        if lower_case:
+            return 'in development'
+        else:
+            return 'In Development'
     elif stat == 'testing':
-        return 'Testing'
+        if lower_case:
+            return 'testing'
+        else:
+            return 'Testing'
     elif stat == 'running':
-        return 'Running'
+        if lower_case:
+            return 'running'
+        else:
+            return 'Running'
     elif stat == 'paused':
-        return 'Paused'
+        if lower_case:
+            return 'paused'
+        else:
+            return 'Paused'
     elif stat == 'finished':
-        return 'Finished'
+        if lower_case:
+            return 'finished'
+        else:
+            return 'Finished'
     else:
-        return 'Unknown'
+        if lower_case:
+            return 'unknown'
+        else:
+            return 'Unknown'
+
+def fancy_status(request, stat, lower_case=False, **kwargs):
+    return tag.p(tag.span(status(stat, lower_case)),
+                 tag.img(src=request.static_url('pyquest:static/img/%s.png' % stat), alt=status(stat, lower_case)),
+                 **kwargs)
