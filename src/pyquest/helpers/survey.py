f39968f33b1c3e619fc03ce855050fadb2069795
mhall <none@none>
2012-03-14 11:41:41 +0000

    Fixed bug: Fancy status was hard-coded.

--- src/pyquest/helpers/survey.py
+++ src/pyquest/helpers/survey.py
@@ -42,18 +42,18 @@ def status(stat, lower_case=False):
 def fancy_status(request, survey, lower_case=False, **kwargs):
     target_status = []
     if survey.status == 'develop':
-        target_status = [tag.li(tag.a('Test', href=request.route_url('survey.status', sid=1, _query={'status': 'testing'}), class_='post-submit', data_confirm='no-confirm')),
-                         tag.li(tag.a('Start', href=request.route_url('survey.status', sid=1, _query={'status': 'running'}), class_='post-submit', data_confirm='no-confirm'))]
+        target_status = [tag.li(tag.a('Test', href=request.route_url('survey.status', sid=survey.id, _query={'status': 'testing'}), class_='post-submit', data_confirm='no-confirm')),
+                         tag.li(tag.a('Start', href=request.route_url('survey.status', sid=survey.id, _query={'status': 'running'}), class_='post-submit', data_confirm='no-confirm'))]
     elif survey.status == 'testing':
-        target_status = [tag.li(tag.a('Stop Test', href=request.route_url('survey.status', sid=1, _query={'status': 'develop'}), class_='post-submit', data_confirm='no-confirm'))]
+        target_status = [tag.li(tag.a('Stop Test', href=request.route_url('survey.status', sid=survey.id, _query={'status': 'develop'}), class_='post-submit', data_confirm='no-confirm'))]
     elif survey.status == 'running':
-        target_status = [tag.li(tag.a('Pause', href=request.route_url('survey.status', sid=1, _query={'status': 'paused'}), class_='post-submit', data_confirm='no-confirm')),
-                         tag.li(tag.a('Finish', href=request.route_url('survey.status', sid=1, _query={'status': 'finished'}), class_='post-submit', data_confirm='no-confirm'))]
+        target_status = [tag.li(tag.a('Pause', href=request.route_url('survey.status', sid=survey.id, _query={'status': 'paused'}), class_='post-submit', data_confirm='no-confirm')),
+                         tag.li(tag.a('Finish', href=request.route_url('survey.status', sid=survey.id, _query={'status': 'finished'}), class_='post-submit', data_confirm='no-confirm'))]
     elif survey.status in 'paused':
-        target_status = [tag.li(tag.a('Restart', href=request.route_url('survey.status', sid=1, _query={'status': 'running'}), class_='post-submit', data_confirm='no-confirm')),
-                         tag.li(tag.a('Finish', href=request.route_url('survey.status', sid=1, _query={'status': 'finished'}), class_='post-submit', data_confirm='no-confirm'))]
+        target_status = [tag.li(tag.a('Restart', href=request.route_url('survey.status', sid=survey.id, _query={'status': 'running'}), class_='post-submit', data_confirm='no-confirm')),
+                         tag.li(tag.a('Finish', href=request.route_url('survey.status', sid=survey.id, _query={'status': 'finished'}), class_='post-submit', data_confirm='no-confirm'))]
     elif survey.status in 'finished':
-        target_status = [tag.li(tag.a('Restart', href=request.route_url('survey.status', sid=1, _query={'status': 'running'}), class_='post-submit', data_confirm='no-confirm'))]
+        target_status = [tag.li(tag.a('Restart', href=request.route_url('survey.status', sid=survey.id, _query={'status': 'running'}), class_='post-submit', data_confirm='no-confirm'))]
     return tag.div(tag.p(tag.span(status(survey.status, lower_case)),
                          tag.img(src=request.static_url('pyquest:static/img/%s.png' % survey.status), alt=status(survey.status, lower_case))),
                    tag.ul(target_status, class_='status-changer', style='display:none;'),
