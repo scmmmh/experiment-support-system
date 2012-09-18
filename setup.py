5f5f7e757aef7c910b8aaed31c932587df9e9cfc
mhall <none@none>
2012-09-18 16:44:45 +0100

    0.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ requires = [
     ]
 
 setup(name='PyQuestionnaire',
-      version='0.0',
+      version='0.2',
       description='PyQuestionnaire',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
