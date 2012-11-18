db0058cd77bca3bc5bc181c1de96929d2f515bee
mark.hall@mail.room3b.eu <mark.hall@mail.room3b.eu>
2012-11-18 14:25:13 +0000

    Version 0.5.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requires = [
     ]
 
 setup(name='PyQuestionnaire',
-      version='0.5',
+      version='0.5.1',
       description='PyQuestionnaire',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
