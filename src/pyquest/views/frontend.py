1156fa088587f05d6b5462dae680f972b8d464d5
Mark <m.mhall@sheffield.ac.uk>
2013-06-24 16:01:07 +0100

    Small bugfix.

--- src/pyquest/views/frontend.py
+++ src/pyquest/views/frontend.py
@@ -178,7 +178,7 @@ class ParticipantManager(object):
         def count_to_end(qsheet, seen = []): # TODO: Cycle detection
             seen.append(qsheet.id)
             if qsheet and qsheet.next:
-                followers = [count_to_end(t.target, seen) for t in qsheet.next if t.target.id not in seen]
+                followers = [count_to_end(t.target, seen) for t in qsheet.next if t.target and t.target.id not in seen]
                 if followers:
                     return max(followers) + 1
                 else:
