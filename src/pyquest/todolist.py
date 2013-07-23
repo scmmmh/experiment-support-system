78ccb240af507e6e04965a52af3fd3009ae12d11
Paul Stephenson <pstephenson@nowhere.org>
2013-07-23 18:50:55 +0100

    Improved calculation of order restrictions. Still not correct all the time though.
    --HG--
    branch : multitask

--- src/pyquest/todolist.py
+++ src/pyquest/todolist.py
@@ -6,27 +6,35 @@ import random
 # between means that between them all users must do all
 
 # within-within
-def ww(tasks, interfaces, generate):
+def ww(tasks, interfaces, taskRestores, interfaceRestores, generate):
     if generate:
         combinations = []
         for task in tasks:
             for interface in interfaces:
                 combinations.append((task, interface))
         print "Combinations before %s" % str(combinations)
-        permutations = [list(i) for i in itertools.permutations(combinations)]
+        rawpermutations = [list(i) for i in itertools.permutations(combinations)]
+        ac = []
+        for tr in taskRestores:
+            for interface in interfaces:
+                ac.append((tr, interface))
+        print "additional combinations before %s" % str(ac)
+        permutations = []
+        for p in rawpermutations:
+            permutations.append(ac + p)
     else:
         permutations = math.factorial(len(tasks) * len(interfaces))
 
     return permutations
 
 # within-between
-def wb(tasks, interfaces, generate):
+def wb(tasks, interfaces, taskRestores, interfaceRestores, generate):
     if generate:
         permutations = []
         for interface in interfaces:
             combinations = []
             for task in tasks:
-                combinations.append((task, interface))
+                combinations.append((taskRestores + task, interface))
             print "Combinations before %s" % str(combinations)
             permutations = permutations + [list(i) for i in itertools.permutations(combinations)]
     else:
@@ -35,13 +43,13 @@ def wb(tasks, interfaces, generate):
     return permutations;
 
 # between-within
-def bw(tasks, interfaces, generate):
+def bw(tasks, interfaces, taskRestores, interfaceRestores, generate):
     if generate:
         permutations = []
         for task in tasks:
             combinations = []
             for interface in interfaces:
-                combinations.append((task, interface))
+                combinations.append((taskRestores + task, interface))
             print "Combinations before %s" % str(combinations)
             permutations = permutations + [list(i) for i in itertools.permutations(combinations)]
     else:
@@ -50,18 +58,18 @@ def bw(tasks, interfaces, generate):
     return permutations
 
 # between-between
-def bb(tasks, interfaces, generate):
+def bb(tasks, interfaces, taskRestores, interfaceRestores, generate):
     if generate:                                                  
         permutations = []
         for task in tasks:
             for interface in interfaces:
-                permutations.append([(task, interface)])
+                permutations.append([(taskRestores + task, interface)])
     else:
         permutations = len(tasks) * len(interfaces)
 
     return permutations
 
-def constructLists(factors, disallow, order):
+def listsWithDisallow(factors, disallow):
     """ Constructs the sublists of combinations needed according to the restrictions passed in. So, for example, if passed 
     ['A', 'B', 'C', 'D', 'E'] as factors and ['AB'] as disallow it will return the two lists ['A', 'C', 'D', 'E']
     and ['B', 'C', 'D', 'E']. It is done as 'to keep' and then 'to remove' in order to handle cases where disallowed 
@@ -70,30 +78,79 @@ def constructLists(factors, disallow, order):
     disallow we need to form the product first - so ['AB', 'CD'] gives four pairs to remove AC AD BC BD.
     """
     lists = []
-    if disallow and len(disallow) > 0 and disallow[0] != '':
-        ctk = list(itertools.product(*disallow))
+    restores = []
+    ctk = list(itertools.product(*disallow))
 #        print "combinations to keep %s " % str(ctk)
-        ctr = []
-        for i in range(len(ctk)):
-            tk = ctk[i]
-            td = disallow
+    ctr = []
+    for i in range(len(ctk)):
+        tk = ctk[i]
+        td = disallow
 #            print "tk %s td %s" % (tk, td)
-            tr = ()
-            for j in range(len(tk)):
-                tr = tr + tuple(td[j].replace(tk[j], ''))
+        tr = ()
+        for j in range(len(tk)):
+            tr = tr + tuple(td[j].replace(tk[j], ''))
 #            print "tr %s td %s" % (tr, td)
-            ctr.append(tr)
+        ctr.append(tr)
 #        print "combinations to remove %s " % str(ctr)
-        for group in ctr:
+    for group in ctr:
 #            print "REMOVING %s" % str(group)
-            factorsClone = list(factors)
-            for d in group:
-                factorsClone.remove(d)
+        factorsClone = list(factors)
+        for d in group:
+            factorsClone.remove(d)
 #            print "APPENDING %s\n" % str(factorsClone)
-            lists.append(factorsClone)
+        lists.append(factorsClone)
+        restores.append([])
+
+    return (restores, lists)
+
+def listsWithOrder(factors, order):
+    """ If passed factors ['A','B', 'C'] and order ['AB'] then we want only those permutations of ABC which have A before B. This
+    can be done recursively by partitioning the factors. We need ['A'] plus the permutations of ['B', 'C'] AND ['C'] plus the result of
+    applying the same process again to ['A', 'B']. The result of this second process in ['A'] plus permutations of ['B']. This function
+    returns two lists and the recursion has only to define how to add them together - restore['C'] + {restore['A'] + permute['B']} equals
+    restore ['C, A'] + permute ['B']. So the total outcome for listsWithOrder(['A', 'B', 'C'], ['AB']) will be:
+
+    restore[0] = ['A'] permute[0] = ['B', 'C']
+    restore[1] = ['C', 'A'] permute[1] = ['B']
+
+    and we compute the final permutations as ['A'] + ['B', 'C'] = ['A', 'B', 'C']
+                                             ['A'] + ['C', 'B'] = ['A', 'C', 'B']
+                                             ['C', 'A'] + ['B'] = ['C', 'A', 'B']
+    """
+    lists = []
+    restores = []
+    for f in factors:
+        if f == order[0][0]:
+            lists.append(factors[1:])
+            restores.append([f])
+        elif f != order[0][1]:
+            factorsClone = list(factors)
+            factorsClone.remove(f)
+            reply = listsWithOrder(factorsClone, order)
+            for i in range(len(reply[0])):
+                restores.append([f] + reply[0][i])
+                lists.append(reply[1][i])
+    return (restores, lists)
+
+
+def constructLists(factors, disallow, order):
+    restores = []
+    lists = []
+    if disallow and len(disallow) > 0 and disallow[0] != '':
+        reply = listsWithDisallow(factors, disallow)
+        restores = reply[0]
+        lists = reply[1]
+    elif order and len(order) > 0 and order[0] != '':
+        reply = listsWithOrder(factors, order)
+        restores = reply[0]
+        lists = reply[1]
     else:
         lists.append(factors)
-    return lists
+        restores.append([])
+
+    for i in range(len(restores)):
+        print "restore %s perm %s" %(str(restores[i]), str(lists[i]))
+    return (restores, lists)
 
 def getPermutations(worb, task_count, interface_count, shuffle, task_disallow, interface_disallow, generate, task_order):
 
@@ -113,8 +170,12 @@ def getPermutations(worb, task_count, interface_count, shuffle, task_disallow, i
     for bit in task_order.split(','):
         tOrder.append(bit)
 
-    taskLists = constructLists(tasks, tDisallow, tOrder)
-    interfaceLists = constructLists(interfaces, iDisallow, None)
+    reply = constructLists(tasks, tDisallow, tOrder)
+    taskRestores = reply[0]
+    taskLists = reply[1]
+    reply = constructLists(interfaces, iDisallow, None)
+    interfaceRestores = reply[0]
+    interfaceLists = reply[1]
 
     if generate:
         toDoList = []
@@ -123,31 +184,29 @@ def getPermutations(worb, task_count, interface_count, shuffle, task_disallow, i
 
     for i in range(len(taskLists)):
         for j in range(len(interfaceLists)):
-            toDoList = toDoList + globals()[worb](taskLists[i], interfaceLists[j], generate)
+                toDoList = toDoList + globals()[worb](taskLists[i], interfaceLists[j], taskRestores[i], interfaceRestores[j], generate)
 
     if generate and shuffle:
         random.shuffle(toDoList)
 
     # First attemp BFI 
     # get the two tasks from 'tOrder'
-    if tOrder and len(tOrder) > 0 and tOrder[0] != '':
-        for order in tOrder:
-            if generate:
-                order = order[::-1]
+    # if tOrder and len(tOrder) > 0 and tOrder[0] != '':
+    #     for order in tOrder:
+    #         if generate:
+    #             order = order[::-1]
 
-                toRemove = []
-                for i in range(len(toDoList)):
+    #             toRemove = []
+    #             for i in range(len(toDoList)):
 
-                    if remove(toDoList[i], 0, order, len(toDoList[i])):
-                        toRemove.append(i)
+    #                 if remove(toDoList[i], 0, order, len(toDoList[i])):
+    #                     toRemove.append(i)
 
-                toRemove.reverse()
-                print "reversed toRemove is %s" % str(toRemove)
+    #             toRemove.reverse()
+    #             print "reversed toRemove is %s" % str(toRemove)
 
-                for i in toRemove:
-                    del toDoList[i]
-            else:
-                toDoList = toDoList / 2
+    #             for i in toRemove:
+    #                 del toDoList[i]
                     
     return toDoList
 
