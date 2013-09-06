9e7ec3009e3491e040945be3c100452640fedecf
Mark <mark.hall@mail.room3b.eu>
2013-09-06 20:13:26 +0100

    Code cleanup
    --HG--
    branch : multitask

--- src/pyquest/__init__.py
+++ src/pyquest/__init__.py
@@ -20,8 +20,10 @@ def main(global_config, **settings):
     config = Configurator(settings=settings, session_factory=session_factory)
     config.add_static_view('static', 'static', cache_max_age=3600)
     l10n.init(settings)
-    template_defaults = {'h': helpers, '_': l10n.get_translator('no', 'translations').ugettext}
-    renderer.init(settings, {'text/html': template_defaults, 'application/xml': template_defaults})
+    template_defaults = {'h': helpers,
+                         '_': l10n.get_translator('no', 'translations').ugettext}
+    renderer.init(settings, {'text/html': template_defaults,
+                             'application/xml': template_defaults})
     views.init(config)
     config.scan()
     Notifier(settings).start()
