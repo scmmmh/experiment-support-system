7209fa06f53f9445d932cb18c9cac1a1249fdc0b
Mark <mark.hall@work.room3b.eu>
2013-09-13 16:20:05 +0100

    Added a function for downgrading a database by one revision.
    --HG--
    branch : multitask

--- src/pyquest/scripts/update.py
+++ src/pyquest/scripts/update.py
@@ -12,9 +12,18 @@ def init(subparsers):
     parser = subparsers.add_parser('update-database', help='Update the PyQuestionnaire database')
     parser.add_argument('configuration', help='PyQuestionnaire configuration file')
     parser.set_defaults(func=update_database)
+    parser = subparsers.add_parser('downgrade-database', help='Downgrade the PyQuestionnaire database')
+    parser.add_argument('configuration', help='PyQuestionnaire configuration file')
+    parser.set_defaults(func=downgrade_database)
     
 def update_database(args):
     alembic_config = config.Config(args.configuration, ini_section='app:main')
     alembic_config.set_section_option('app:main', 'script_location', 'pyquest:migrations')
     alembic_config.set_section_option('app:main', 'url', 'hm')
     command.upgrade(alembic_config, DB_VERSION)
+
+def downgrade_database(args):
+    alembic_config = config.Config(args.configuration, ini_section='app:main')
+    alembic_config.set_section_option('app:main', 'script_location', 'pyquest:migrations')
+    alembic_config.set_section_option('app:main', 'url', 'hm')
+    command.downgrade(alembic_config, '-1')
