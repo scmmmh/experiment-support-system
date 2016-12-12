3e45e31cccfbec10d45ab9bc0b685f8e0c585f47
Mark <Mark.Hall@edgehill.ac.uk>
2016-12-12 17:21:12 +0000

    Fixed the command-line scripts.
    --HG--
    branch : rewrite

--- src/ess/scripts/configuration.py
+++ src/ess/scripts/configuration.py
@@ -5,29 +5,34 @@ Script used to generate a configuration file.
 .. moduleauthor:: Mark Hall <mark.hall@mail.room3b.eu>
 """
 import uuid
-from genshi.template import TemplateLoader, loader, NewTextTemplate
 
-from pyquest.scripts.main import get_user_parameter
+from kajiki import TextTemplate
+from pkg_resources import resource_string
+
+from .main import get_user_parameter
+
 
 def init(subparsers):
     parser = subparsers.add_parser('generate-config', help='Generate the Experiment Support System configuration file')
     parser.add_argument('--filename', default='production.ini', help='Configuration file name')
     parser.add_argument('--sqla-connection-string', default=None, help='SQLAlchemy database connection string')
+    parser.add_argument('--debug', default=False, action='store_true', help='Set the debug flags in the configuration')
     parser.set_defaults(func=generate_config)
+
     
 def generate_config(args):
     '''Generates a configuration file based on the default_config.txt template.
     '''
-    tmpl_loader = TemplateLoader([loader.package('pyquest', 'scripts/templates/')])
-    tmpl = tmpl_loader.load('default_config.txt', cls=NewTextTemplate)
+    tmpl = TextTemplate(resource_string('ess', 'scripts/templates/default_config.txt').decode('utf-8'))
     params = {'encrypt_key': uuid.uuid1(),
-              'validate_key': uuid.uuid1()}
+              'validate_key': uuid.uuid1(),
+              'debug': args.debug}
     if args.sqla_connection_string:
         params['sqlalchemy_url'] = args.sqla_connection_string
     else:
-        params['sqlalchemy_url'] = get_user_parameter('SQL Alchemy Connection String', 'sqlite:///%(here)s/ess_test.db')
-    
+        params['sqlalchemy_url'] = get_user_parameter('SQL Alchemy Connection String',
+                                                      'sqlite:///%(here)s/eregisters_test.db')
+
     with open(args.filename, 'w') as out_f:
-        for data in tmpl.generate(**params).render('text'):
-            out_f.write(data)
+        out_f.write(tmpl(params).render())
     
--- src/ess/scripts/populate.py
+++ src/ess/scripts/populate.py
@@ -7,22 +7,17 @@ from csv import DictReader
 from lxml import etree
 from pkg_resources import resource_stream
 from pyramid.paster import (get_appsettings, setup_logging)
+from pywebtools.sqlalchemy import DBSession, Base
+from pywebtools.pyramid.auth.models import User, PermissionGroup, Permission
 from sqlalchemy import engine_from_config
 
-from ess.models import (DBSession, Base, Experiment, QSheet, DataItem,
-                            DataItemAttribute, User,
-                            QSheetAttribute, QSheetTransition,
-                            DataSet, DataSetAttributeKey, Notification, PermutationSet,
-                            DataSetRelation)
 
 def init(subparsers):
     parser = subparsers.add_parser('initialise-database', help='Initialise the database')
     parser.add_argument('configuration', help='Experiment Support System configuration file')
     parser.add_argument('--drop-existing', action='store_true', default=False, help='Drop any existing tables')
     parser.set_defaults(func=initialise_database)
-    parser = subparsers.add_parser('load-sample-data', help='Loads the sample data')
-    parser.add_argument('configuration', help='Experiment Support System configuration file')
-    parser.set_defaults(func=load_test_data)
+
 
 def initialise_database(args):
     settings = get_appsettings(args.configuration)
@@ -34,48 +29,23 @@ def initialise_database(args):
     Base.metadata.create_all(engine)
     dbsession = DBSession()
     with transaction.manager:
-        user = User(u'admin', u'admin@example.com', u'Admin', u'password')
-        group = Group(title='Site administrator')
-        group.permissions.append(Permission(name='admin.users', title='Administer the users'))
-        group.permissions.append(Permission(name='admin.groups', title='Administer the permission groups'))
-        group.permissions.append(Permission(name='admin.question_types', title='Administer the question types'))
-        user.groups.append(group)
-        group = Group(title='Developer')
-        group.permissions.append(Permission(name='survey.new', title='Create new experiments'))
-        user.groups.append(group)
+        user = User(email='admin@example.com', display_name='Admin')
+        user.new_password('password')
+        group = PermissionGroup(title='Site administrator')
+        group.permissions.append(Permission(name='admin.users.view', title='View all users'))
+        group.permissions.append(Permission(name='admin.users.edit', title='Edit all users'))
+        group.permissions.append(Permission(name='admin.users.delete', title='Delete all users'))
+        group.permissions.append(Permission(name='admin.users.permissions', title='Edit all user\'s permissions'))
+        user.permission_groups.append(group)
+        group = PermissionGroup(title='Developer')
+        group.permissions.append(Permission(name='experiment.create', title='Create new experiments'))
+        user.permission_groups.append(group)
         dbsession.add(user)
-        group = Group(title='Content administrator')
-        group.permissions.append(Permission(name='survey.view-all', title='View all experiments'))
-        group.permissions.append(Permission(name='survey.edit-all', title='Edit all experiments'))
-        group.permissions.append(Permission(name='survey.delete-all', title='Delete all experiments'))
+        group = PermissionGroup(title='Content administrator')
+        group.permissions.append(Permission(name='experiment.view', title='View all experiments'))
+        group.permissions.append(Permission(name='experiment.edit', title='Edit all experiments'))
+        group.permissions.append(Permission(name='experiment.delete', title='Delete all experiments'))
         dbsession.add(group)
-        element = XmlValidator().to_python(resource_stream('pyquest', 'scripts/templates/default_question_types.xml').read())
-        dbsession.add(load_q_types_from_xml(dbsession, element, 0))
     alembic_config = config.Config(args.configuration, ini_section='app:main')
-    alembic_config.set_section_option('app:main', 'script_location', 'pyquest:migrations')
+    alembic_config.set_section_option('app:main', 'script_location', 'ess:migrations')
     command.stamp(alembic_config, "head")
-
-def load_test_data(args):
-    settings = get_appsettings(args.configuration)
-    setup_logging(args.configuration)
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    dbsession = DBSession()
-    with transaction.manager:
-        user = dbsession.query(User).first()
-        
-        # Sample Survey 1
-        survey = load_survey_from_stream(resource_stream('pyquest', 'scripts/templates/sample_survey_1.exp'), user, dbsession)
-        notification = Notification(ntype='interval', value=1, recipient=user.email)
-        survey.notifications.append(notification)
-        
-        # Sample Survey 2
-        survey = load_survey_from_stream(resource_stream('pyquest', 'scripts/templates/sample_survey_2.exp'), user, dbsession)
-        notification = Notification(ntype='pcount', value=10, recipient=user.email)
-        survey.notifications.append(notification)
-
-        # Sample Survey 3
-        load_survey_from_stream(resource_stream('pyquest', 'scripts/templates/sample_survey_3.exp'), user, dbsession)
-
-        # SURVEY 4
-        load_survey_from_stream(resource_stream('pyquest', 'scripts/templates/sample_survey_4.exp'), user, dbsession)
