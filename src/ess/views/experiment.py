71bdcb73ac1729e8d36a165c66d7d08cc13e4706
Mark <Mark.Hall@edgehill.ac.uk>
2016-12-12 17:49:36 +0000

    Made layouts consistent.
    --HG--
    branch : rewrite

--- src/ess/views/experiment.py
+++ src/ess/views/experiment.py
@@ -1,6 +1,13 @@
 from pyramid.view import view_config
-from pywebtools.pyramid.auth.views import current_user
+from pywebtools.pyramid.auth.views import current_user, require_permission
 
 
 def init(config):
     config.add_route('experiment.create', '/experiments/create')
+
+
+@view_config(route_name='experiment.create', renderer='ess:templates/experiment/create.kajiki')
+@current_user()
+@require_permission(permission='experiment.create')
+def create(request):
+    return {}
