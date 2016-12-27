b7790e993a21066c49fe4ace2665b44682536582
Mark <Mark.Hall@work.room3b.eu>
2016-12-27 10:54:45 +0000

    Cleaned up gulpfile.
    --HG--
    branch : rewrite

--- gulpfile.js
+++ gulpfile.js
@@ -11,112 +11,150 @@ var uglify = require('gulp-uglify');
 var pump = require('pump');
 
 gulp.task('default', ['scss', 'js']);
+gulp.task('scss', ['scss-core', 'material-design-icons']);
+gulp.task('js', ['library-js', 'frontend-js', 'backend-js']);
+gulp.task('library-js', ['what-input', 'modernizr', 'jquery', 'jquery-ui', 'foundation-js']);
 
-// Task to build the CSS
-gulp.task('scss', function(cb) {
-    // Build the CSS
+gulp.task('scss-core', function(cb) {
     var css_sources = ['src/static/scss/base-settings.scss'];
     css_sources.push('src/static/scss/foundation-loader.scss');
     css_sources.push('src/static/scss/application.scss');
-    gulp.src(css_sources, {base: 'src/static/scss'}).
-        pipe(concat('application.scss')).
-        pipe(sass({
+    pump([
+        gulp.src(css_sources, {base: 'src/static/scss'}),
+        concat('application.scss'),
+        sass({
             includePaths: ['node_modules/foundation-sites/scss',
                            'node_modules/foundation-icons']
-        })).
-        pipe(clean_css()).
-        pipe(rename('application.min.css')).
-        pipe(gulp.dest('src/ess/static/css'));
-    // Copy Material Design Icon Fonts
-    gulp.src(['node_modules/material-design-icons/iconfont/*.eot',
-              'node_modules/material-design-icons/iconfont/*.svg',
-              'node_modules/material-design-icons/iconfont/*.ttf',
-              'node_modules/material-design-icons/iconfont/*.woff',
-              'node_modules/material-design-icons/iconfont/*.woff2'], {base: 'node_modules/material-design-icons/iconfont'}).
-        pipe(gulp.dest('src/ess/static/css'));
+        }),
+        clean_css(),
+        rename('application.min.css'),
+        gulp.dest('src/ess/static/css')
+    ], cb);
 });
 
-// Task to build the JavaScript files
-gulp.task('js', function(cb) {
-    var count = 0;
-    var pcb = function() {
-        count++;
-        if(count == 5) {
-            cb();
-        }
-    }
-    /* Build library files */
-    pump([gulp.src(['node_modules/foundation-sites/js/foundation.core.js',
-                    'node_modules/foundation-sites/js/foundation.util.*.js',
-                    'node_modules/foundation-sites/js/foundation.abide.js',
-                    'node_modules/foundation-sites/js/foundation.accordion.js',
-                    'node_modules/foundation-sites/js/foundation.accordionMenu.js',
-                    'node_modules/foundation-sites/js/foundation.drilldown.js',
-                    'node_modules/foundation-sites/js/foundation.dropdown.js',
-                    'node_modules/foundation-sites/js/foundation.dropdownMenu.js',
-                    'node_modules/foundation-sites/js/foundation.equalizer.js',
-                    'node_modules/foundation-sites/js/foundation.interchange.js',
-                    'node_modules/foundation-sites/js/foundation.magellan.js',
-                    'node_modules/foundation-sites/js/foundation.offcanvas.js',
-                    'node_modules/foundation-sites/js/foundation.responsiveMenu.js',
-                    'node_modules/foundation-sites/js/foundation.responsiveToggle.js',
-                    'node_modules/foundation-sites/js/foundation.reveal.js',
-                    'node_modules/foundation-sites/js/foundation.slider.js',
-                    'node_modules/foundation-sites/js/foundation.sticky.js',
-                    'node_modules/foundation-sites/js/foundation.tabs.js',
-                    'node_modules/foundation-sites/js/foundation.toggler.js',
-                    'node_modules/foundation-sites/js/foundation.tooltip.js',]),
-          babel({
-              presets: ['es2015']
-          }),
-          concat('foundation.js'),
-          uglify(),
-          gulp.dest('src/ess/static/js')
-    ], pcb);
-    pump([modernizr({
-              options: ['html5shiv',
-                        'html5printshiv',
-                        'setClasses']
-          }),
-          gulp.src(['node_modules/jquery/dist/jquery.js',
-                    'node_modules/what-input/what-input.js']),
-          uglify(),
-          gulp.dest('src/ess/static/js')
-    ], pcb);
-    pump([gulp.src(['node_modules/jquery-ui/ui/version.js',
-                    'node_modules/jquery-ui/ui/effect.js',
-                    'node_modules/jquery-ui/ui/plugin.js',
-                    'node_modules/jquery-ui/ui/widget.js',
-                    'node_modules/jquery-ui/ui/position.js',
-                    'node_modules/jquery-ui/ui/data.js',
-                    'node_modules/jquery-ui/ui/safe-active-element.js',
-                    'node_modules/jquery-ui/ui/safe-blur.js',
-                    'node_modules/jquery-ui/ui/disable-selection.js',
-                    'node_modules/jquery-ui/ui/scroll-parent.js',
-                    'node_modules/jquery-ui/ui/widgets/mouse.js',
-                    'node_modules/jquery-ui/ui/widgets/draggable.js',
-                    'node_modules/jquery-ui/ui/widgets/dropable.js',
-                    'node_modules/jquery-ui/ui/widgets/sortable.js']),
-          concat('jquery-ui.js'),
-          uglify(),
-          gulp.dest('src/ess/static/js')
-    ], pcb);
-    /* Build frontend files */
-    pump([gulp.src('src/static/js/frontend/*.js'),
-          concat('frontend.js'),
-          uglify(),
-          gulp.dest('src/ess/static/js')
-    ], pcb);
-    /* Build backend files */
-    pump([gulp.src('src/static/js/backend/*.js'),
-          concat('backend.js'),
-          uglify(),
-          gulp.dest('src/ess/static/js')
-    ], pcb);
+gulp.task('material-design-icons', function(cb) {
+    pump([
+        gulp.src([
+            'node_modules/material-design-icons/iconfont/*.eot',
+            'node_modules/material-design-icons/iconfont/*.svg',
+            'node_modules/material-design-icons/iconfont/*.ttf',
+            'node_modules/material-design-icons/iconfont/*.woff',
+            'node_modules/material-design-icons/iconfont/*.woff2'],
+            {base: 'node_modules/material-design-icons/iconfont'}),
+        gulp.dest('src/ess/static/css')
+    ], cb);
+});
+
+gulp.task('foundation-js', function(cb) {
+    pump([
+        gulp.src([
+            'node_modules/foundation-sites/js/foundation.core.js',
+            'node_modules/foundation-sites/js/foundation.util.*.js',
+            'node_modules/foundation-sites/js/foundation.abide.js',
+            'node_modules/foundation-sites/js/foundation.accordion.js',
+            'node_modules/foundation-sites/js/foundation.accordionMenu.js',
+            'node_modules/foundation-sites/js/foundation.drilldown.js',
+            'node_modules/foundation-sites/js/foundation.dropdown.js',
+            'node_modules/foundation-sites/js/foundation.dropdownMenu.js',
+            'node_modules/foundation-sites/js/foundation.equalizer.js',
+            'node_modules/foundation-sites/js/foundation.interchange.js',
+            'node_modules/foundation-sites/js/foundation.magellan.js',
+            'node_modules/foundation-sites/js/foundation.offcanvas.js',
+            'node_modules/foundation-sites/js/foundation.responsiveMenu.js',
+            'node_modules/foundation-sites/js/foundation.responsiveToggle.js',
+            'node_modules/foundation-sites/js/foundation.reveal.js',
+            'node_modules/foundation-sites/js/foundation.slider.js',
+            'node_modules/foundation-sites/js/foundation.sticky.js',
+            'node_modules/foundation-sites/js/foundation.tabs.js',
+            'node_modules/foundation-sites/js/foundation.toggler.js',
+            'node_modules/foundation-sites/js/foundation.tooltip.js'
+        ]),
+        babel({
+            presets: ['es2015']
+        }),
+        concat('foundation.js'),
+        uglify(),
+        gulp.dest('src/ess/static/js')
+    ], cb);
+});
+
+gulp.task('modernizr', function(cb) {
+    pump([
+        modernizr({
+            options: [
+                'html5shiv',
+                'html5printshiv',
+                'setClasses']
+        }),
+        uglify(),
+        gulp.dest('src/ess/static/js')
+    ], cb);
+});
+
+gulp.task('jquery', function(cb) {
+    pump([
+        gulp.src([
+            'node_modules/jquery/dist/jquery.js',
+        ]),
+        uglify(),
+        gulp.dest('src/ess/static/js')
+    ], cb);
+});
+
+gulp.task('what-input', function(cb) {
+    pump([
+        gulp.src([
+            'node_modules/what-input/what-input.js'
+        ]),
+        uglify(),
+        gulp.dest('src/ess/static/js')
+    ], cb);
+});
+
+gulp.task('jquery-ui', function(cb) {
+    pump([
+        gulp.src([
+            'node_modules/jquery-ui/ui/version.js',
+            'node_modules/jquery-ui/ui/effect.js',
+            'node_modules/jquery-ui/ui/plugin.js',
+            'node_modules/jquery-ui/ui/widget.js',
+            'node_modules/jquery-ui/ui/position.js',
+            'node_modules/jquery-ui/ui/data.js',
+            'node_modules/jquery-ui/ui/safe-active-element.js',
+            'node_modules/jquery-ui/ui/safe-blur.js',
+            'node_modules/jquery-ui/ui/disable-selection.js',
+            'node_modules/jquery-ui/ui/scroll-parent.js',
+            'node_modules/jquery-ui/ui/widgets/mouse.js',
+            'node_modules/jquery-ui/ui/widgets/draggable.js',
+            'node_modules/jquery-ui/ui/widgets/dropable.js',
+            'node_modules/jquery-ui/ui/widgets/sortable.js'
+        ]),
+        concat('jquery-ui.js'),
+        uglify(),
+        gulp.dest('src/ess/static/js')
+    ], cb);
+});
+
+gulp.task('frontend-js', function(cb) {
+    pump([
+        gulp.src('src/static/js/frontend/*.js'),
+        concat('frontend.js'),
+        uglify(),
+        gulp.dest('src/ess/static/js')
+    ], cb);
+});
+
+gulp.task('backend-js', function(cb) {
+    pump([
+        gulp.src('src/static/js/backend/*.js'),
+        concat('backend.js'),
+        uglify(),
+        gulp.dest('src/ess/static/js')
+    ], cb);
 });
 
 // Task to watch the SCSS/JS files and re-build when needed
 gulp.task('watch', function() {
-    gulp.watch('src/static/scss/**/*.scss', ['scss']);
-    gulp.watch('src/static/js/**/*.js', ['js']);
+    gulp.watch('src/static/scss/**/*.scss', ['scss-core']);
+    gulp.watch('src/static/js/**/*.js', ['frontend-js', 'backend-js']);
 });
