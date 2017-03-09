55ad5a7718f694e261292beb2c5dc0f1d71651b9
Mark <Mark.Hall@edgehill.ac.uk>
2017-03-09 13:14:10 +0000

    Show the live, public experiments on the landing page.
    --HG--
    branch : rewrite

--- src/ess/views/__init__.py
+++ src/ess/views/__init__.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 from pywebtools.pyramid.auth.views import current_user
 from pywebtools.sqlalchemy import DBSession
+from sqlalchemy import and_
 
 from . import frontend, user, experiment, page, data, results
 from ess.models import Experiment
@@ -20,7 +21,10 @@ def init(config):
 @view_config(route_name='root', renderer='ess:templates/root.kajiki')
 @current_user()
 def root(request):
-    return {}
+    dbsession = DBSession()
+    experiments = dbsession.query(Experiment).filter(and_(Experiment.status == 'live',
+                                                          Experiment.public == True)).order_by(Experiment.title)
+    return {'experiments': experiments}
 
 
 @view_config(route_name='dashboard', renderer='ess:templates/dashboard.kajiki')
