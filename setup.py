e8b07e09ce6d64d3bf01a28920e98fac61141555
Mark <Mark.Hall@work.room3b.eu>
2017-05-31 15:19:26 +0100

    Version 1.0.0a8

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requires = [
     ]
 
 setup(name='ExperimentSupportSystem',
-      version='1.0.0a7',
+      version='1.0.0a8',
       description='Experiment Support System',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
