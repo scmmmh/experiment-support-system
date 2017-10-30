aec13ebbf80e3de63ee5507fa3d9dc5a811cd311
Mark <Mark.Hall@edgehill.ac.uk>
2017-10-30 09:57:51 +0000

    Version 1.0.0a12

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requires = [
     ]
 
 setup(name='ExperimentSupportSystem',
-      version='1.0.0a11',
+      version='1.0.0a12',
       description='Experiment Support System',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
