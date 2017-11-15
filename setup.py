cc6dfd5a31321d46faf8ced0bf27da9081394b9e
Mark <Mark.Hall@work.room3b.eu>
2017-11-15 13:44:09 +0000

    Remove explicit marshmallow-jsonapi dependency

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ requires = [
     'pycrypto',
     'asset',
     'pywebtools>=1.1.3',
-    'marshmallow_jsonapi==0.15.1-mh',
+    'marshmallow_jsonapi>=0.15.1',
     'marshmallow<3'
     ]
 
 setup(name='ExperimentSupportSystem',
-      version='1.0.0a12',
+      version='1.0.0a13',
       description='Experiment Support System',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
