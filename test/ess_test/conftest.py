a929b91a1b37c19158d3e16c2c009aaf8a7cd9c3
Mark <Mark.Hall@work.room3b.eu>
2017-11-18 14:45:10 +0000

    Updated the test fixtures an root tests to work with the pywebtools testing module

--- test/ess_test/conftest.py
+++ test/ess_test/conftest.py
@@ -5,28 +5,25 @@
 ###########################################
 
 This module provides a series of fixtures that can be used to have a fully configured
-database session (:func:`~ess_test.conftest.database`), WSGI application (:func:`~ess_test.conftest.app`,
-or :class:`~ess_test.conftest.FunctionalTester` (:func:`~ess_test.conftest.functional_tester`).
+database session (:func:`~ess_test.conftest.database`) and :class:`~ess_test.conftest.DatabaseTester`
+via the fixture :func:`~ess_test.conftest.database_tester`.
 
 .. moduleauthor:: Mark Hall <mark.hall@work.room3b.eu>
 """
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
-import pyquest
 import pytest
 import transaction
 
 from alembic import config, command
 from pkg_resources import resource_stream
 from pyramid.paster import (get_app, get_appsettings, setup_logging)
-from sqlalchemy import engine_from_config
+from pywebtools.pyramid.auth.models import (User, PermissionGroup, Permission)
+from pywebtools.sqlalchemy import (DBSession, Base)
+from sqlalchemy import engine_from_config, text
 from webtest import TestApp
 
 from ess import models
-from ess.models import (DBSession, Base, User, Group, Permission)
-from pyquest import models as old_models
-from pyquest.validation import XmlValidator
-from pyquest.views.admin.question_types import load_q_types_from_xml
 
 dbsession_initialised = False
 
@@ -52,7 +49,6 @@ def database():
     engine = engine_from_config(settings, 'sqlalchemy.')
     if not dbsession_initialised:
         DBSession.configure(bind=engine)
-        old_models.DBSession.configure(bind=engine)
         dbsession_initialised = True
     Base.metadata.create_all(engine)
 
@@ -68,35 +64,35 @@ def database():
 
     # Create Test Users
     with transaction.manager:
-        admin_user = User('admin', 'admin@example.com', 'Admin', 'password')
-        developer_user = User('developer', 'developer@example.com', 'Developer', 'password')
-        content_user = User('content', 'content@example.com', 'Content', 'password')
-        general_user = User('general', 'general@example.com', 'General', 'password')
+        admin_user = User(email='admin@example.com', display_name='Admin', password='password')
+        developer_user = User(email='developer@example.com', display_name='Developer', password='password')
+        content_user = User(email='content@example.com', display_name='Content', password='password')
+        general_user = User(email='general@example.com', display_name='General', password='password')
         dbsession.add(general_user)
-        group = Group(title='Site administrator')
+        group = PermissionGroup(title='Site administrator')
         group.permissions.append(Permission(name='admin.users', title='Administer the users'))
         group.permissions.append(Permission(name='admin.groups', title='Administer the permission groups'))
         group.permissions.append(Permission(name='admin.question_types', title='Administer the question types'))
-        admin_user.groups.append(group)
-        group = Group(title='Developer')
+        admin_user.permission_groups.append(group)
+        group = PermissionGroup(title='Developer')
         group.permissions.append(Permission(name='survey.new', title='Create new experiments'))
-        admin_user.groups.append(group)
-        developer_user.groups.append(group)
-        group = Group(title='Content administrator')
+        admin_user.permission_groups.append(group)
+        developer_user.permission_groups.append(group)
+        group = PermissionGroup(title='Content administrator')
         group.permissions.append(Permission(name='survey.view-all', title='View all experiments'))
         group.permissions.append(Permission(name='survey.edit-all', title='Edit all experiments'))
         group.permissions.append(Permission(name='survey.delete-all', title='Delete all experiments'))
-        content_user.groups.append(group)
+        content_user.permission_groups.append(group)
         dbsession.add(admin_user)
         dbsession.add(developer_user)
         dbsession.add(content_user)
-        element = XmlValidator().to_python(resource_stream('pyquest',
-                                                           'scripts/templates/default_question_types.xml').read())
-        dbsession.add(load_q_types_from_xml(dbsession, element, 0))
+        #element = XmlValidator().to_python(resource_stream('pyquest',
+        #                                                   'scripts/templates/default_question_types.xml').read())
+        #dbsession.add(load_q_types_from_xml(dbsession, element, 0))
 
     # Alembic Stamp
     alembic_config = config.Config('testing.ini', ini_section='app:main')
-    alembic_config.set_section_option('app:main', 'script_location', 'pyquest:migrations')
+    alembic_config.set_section_option('app:main', 'script_location', 'ess:migrations')
     command.stamp(alembic_config, "head")
 
     DBSession.remove()
@@ -104,78 +100,28 @@ def database():
     yield DBSession
 
 
-@pytest.yield_fixture
-def app(database):
-    """Fixture that provides a newly initialised ESS pyramid application
-    with an empty database.
-    """
-    # Create the application
-    app = get_app('testing.ini')
-
-    # Yield to the test
-    yield app
-
-
-class RequestTesterMixin(object):
-    """The :class:`~ess_test.conftest.RequestTesterMixin` provides functionality for making requests
-    via the :class:`~ess_test.conftest.FunctionalTester`.
-    """
-
-    def get_url(self, url, headers=None):
-        """Send a GET request to the given ``url``.
-
-        :param url: The url to request
-        :type url: ``unicode``
-        :param headers: Optional headers to send with the request
-        :type headers: ``dict`` or ``list``
-        """
-        if headers:
-            if isinstance(headers, dict):
-                if 'Accept' not in headers:
-                    headers['Accept'] = '*/*'
-            elif isinstance(headers, list):
-                if 'Accept' not in dict(headers):
-                    headers.append(('Accept', '*/*'))
-        else:
-            headers = [('Accept', '*/*')]
-        if isinstance(headers, dict):
-            headers = [(k.encode('utf-8'), v.encode('utf-8')) for k, v in headers.items()]
-        elif isinstance(headers, list):
-            headers = [(k.encode('utf-8'), v.encode('utf-8')) for k, v in headers]
-        self._response = self._test.get(url,
-                                        headers=headers)
-
-    def has_text(self, text):
-        """Check whether the last response contains the given ``text``.
-
-        :param text: The text to look for
-        :type text: ``unicode``
-        """
-        if self._response:
-            assert text in self._response.body
-        else:
-            assert False, 'No request sent'
-
-
-class DBTesterMixin(object):
-    """The :class:`~ess_test.conftest.DBTesterMixin` provides functionality for interacting with the
-    database via the :class:`~ess_test.conftest.FunctionalTester`.
+class DBTester(object):
+    """The :class:`~ess_test.conftest.DBTester` provides functionality for interacting with the
+    database..
     """
 
     def __init__(self):
         self._dbsession = DBSession()
 
-    def get_model(self, name, query):
+    def get_model(self, name, query = None):
         """Retrieve a single instance of the model ``name``, filtered by the ``query``.
 
         :param name: The name of the model to get the instance for
         :type name: ``unicode``
-        :param query: The query to use for selecting the instance
-        :type query: ``unicode``
+        :param query: The query to use for selecting the instance as a tuple of ``(field, comparator, value)``
+        :type query: ``tuple``
         :return: The result of the query
         """
         cls = getattr(models, name)
-        return self._dbsession.query(cls).filter(query).first()
+        query = self._dbsession.query(cls)
+        if query[1] == '==':
+            query = query.filter(getattr(cls, query[0]) == query[2])
+        return query.first()
 
     def create_model(self, name, params):
         """Create a new instance of the model ``name`` with the given ``params``.
@@ -194,25 +140,7 @@ class DBTesterMixin(object):
         return model
 
 
-class FunctionalTester(RequestTesterMixin, DBTesterMixin):
-    """The :class:`~ess_test.conftest.FunctionalTester` provides an easy interface for running
-    functional tests. It mixes in :class:`~ess_test.conftest.RequestTesterMixin` and
-    :class:`~ess_test.conftest.DBTesterMixin` to provide the actual functionality.
-
-    The :class:`~ess_test.conftest.FunctionalTester` should always be created via the
-    :cunc:`~ess_test.conftest.functional_tester` fixture.
-    """
-
-    def __init__(self, app):
-        self._app = app
-        self._test = TestApp(app)
-        self._response = None
-        DBTesterMixin.__init__(self)
-
-
 @pytest.yield_fixture
-def functional_tester(app, database):
-    """Fixture that provides a :class:`~ess_test.conftest.FunctionalTester`.
-    """
-    tester = FunctionalTester(app)
+def database_tester(database):
+    tester = DBTester()
     yield tester
--- test/ess_test/views_test/root_test.py
+++ test/ess_test/views_test/root_test.py
@@ -9,31 +9,30 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 
-def test_root_no_public_experiments(functional_tester):
+def test_root_no_public_experiments(pyramid_app_tester, database_tester):
     """Tests the root view with no publicly listed experiments.
     """
-    functional_tester.get_url('/')
-    functional_tester.has_text('Experiment Support System')
-    functional_tester.has_text('There are currently no experiments available for participation')
-    functional_tester.create_model('Experiment', {'title': 'This is a test experiment',
-                                                  'status': 'running',
-                                                  'public': False,
-                                                  'owned_by': functional_tester.get_model('User',
-                                                                                          "username == 'admin'").id})
-    functional_tester.get_url('/')
-    functional_tester.has_text('Experiment Support System')
-    functional_tester.has_text('There are currently no experiments available for participation')
+    app, db = pyramid_app_tester, database_tester
+    app.goto('/')
+    app.has_text('Experiment Support System')
+    app.not_has_text('There are currently no experiments available for participation')
+    db.create_model('Experiment', {'title': 'This is a test experiment',
+                                   'status': 'live',
+                                   'public': False,
+                                   'owned_by':db.get_model('User', ('email', '==', 'admin@example.com')).id})
+    app.goto('/')
+    app.has_text('Experiment Support System')
+    app.not_has_text('There are currently no experiments available for participation')
 
 
-def test_root_public_experiments(functional_tester):
+def test_root_public_experiments(pyramid_app_tester, database_tester):
     """Tests the root view with a publicly listed experiment.
     """
-    functional_tester.create_model('Experiment', {'title': 'This is a test experiment',
-                                                  'status': 'running',
-                                                  'public': True,
-                                                  'owned_by': functional_tester.get_model('User',
-                                                                                          "username == 'admin'").id})
-    functional_tester.get_url('/')
-    functional_tester.has_text('Experiment Support System')
-    functional_tester.has_text('The following experiments are currently available for participation')
-    functional_tester.has_text('This is a test experiment')
+    app, db = pyramid_app_tester, database_tester
+    db.create_model('Experiment', {'title': 'This is a test experiment',
+                                   'status': 'live',
+                                   'public': True,
+                                    'owned_by': db.get_model('User', ('email', '==', 'admin@example.com')).id})
+    app.goto('/')
+    app.has_text('Experiment Support System')
+    app.has_text('This is a test experiment')
